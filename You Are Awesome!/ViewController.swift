01940f27569fa0efaa31121e3b978233e143f8aa
Junseo Park <parkaph@bc.edu>
2022-01-24 13:53:58 -0500

    Added you are awesome and use of kanye quote api

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,13 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+struct Quote: Decodable{
+    let quote: String
+}
 
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var messageCounter: UILabel!
+    @IBOutlet weak var messageQuote: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad ran...")
+        messageLabel.text = "Amazeballs"
+        messageCounter.text = ""
+        
     }
+    
+    var counter = 0
+    @IBAction func messageButton(_ sender: UIButton) {
+        print("Button Pressed...")
+        counter = counter + 1
+        messageLabel.text = "You are Awesome!"
+        messageLabel.textColor = UIColor.gray
+        messageCounter.text = "Button pressed " + String(counter) + " times"
+        messageCounter.textColor = UIColor.purple
+    }
+    
+    @IBAction func quoteButton(_ sender: UIButton) {
+        let urlString = "https://api.kanye.rest/"
+        guard let url = URL(string: urlString) else {return}
 
+        URLSession.shared.dataTask(with: url) { [self](data, response, error) in
+            guard let data = data else {return}
+            _ = String(data: data, encoding: .utf8)
+            do {
+                let quote = try JSONDecoder().decode(Quote.self, from: data)
+                print(quote.quote)
+                DispatchQueue.main.async {messageQuote.text = quote.quote + " -Kanye"}
+                
+            }
+            catch let jsonErr{
+                print("Error json ", jsonErr)
+            }
+            
+        }.resume()
+        
+    }
+    
 
 }
+
+
+
 
